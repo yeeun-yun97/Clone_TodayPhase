c302565bcc826fd8df290c0450884809ce48b71d
yyn9704@gmail.com <yyn9704@gmail.com>
2022-03-30 23:28:45 +0900

    0330 init

---
+++ app/src/main/java/com/github/yeeun_yun97/clone/today_phase/Quote.kt
@@ -0,0 +1,43 @@
+package com.github.yeeun_yun97.clone.today_phase
+
+import android.content.SharedPreferences
+
+data class Quote(var idx: Int, var text: String, var from: String = "") {
+    companion object {
+        fun saveToPreference(
+            pref: SharedPreferences,
+            idx: Int,
+            text: String,
+            from: String = ""
+        ): Quote {
+            val editor = pref.edit()
+            editor.putString("$idx.text", text)
+            editor.putString("$idx.from", from.trim())
+
+            editor.apply()
+            return Quote(idx, text, from)
+        }
+
+        fun getQuotesFromPreference(pref: SharedPreferences):MutableList<Quote>{
+            val quotes = mutableListOf<Quote>()
+
+            for(i in 0 until 20){
+                if(pref.contains("$i.text")){
+                    val quoteText= pref.getString("$i.text","")!!
+                    val quoteFrom=pref.getString("$i.from","")!!
+                    if(quoteText.isNotBlank()) quotes.add(Quote(i,quoteText,quoteFrom))
+                }
+            }
+            return quotes
+        }
+
+
+        fun removeQuoteFromPreference(pref: SharedPreferences, idx: Int) {
+            val editor = pref.edit()
+
+            editor.remove("$idx.text")
+            editor.remove("$idx.from")
+            editor.apply()
+        }
+    }
+}
---
+++ app/src/main/java/com/github/yeeun_yun97/clone/today_phase/QuoteMainActivity.kt
@@ -0,0 +1,63 @@
+package com.github.yeeun_yun97.clone.today_phase
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.util.*
+
+class QuoteMainActivity : AppCompatActivity() {
+
+    //view comps
+    private val quoteTextView: TextView by lazy {findViewById(R.id.QuoteMainActivity_quoteTextView)}
+    private val quoteAuthorTextView: TextView by lazy {findViewById(R.id.QuoteMainActivity_quoteAuthorTextView)}
+    private val viewQuoteButton : Button by lazy {findViewById(R.id.QuoteMainActivity_viewQuoteButton)}
+    private val editQuoteTextView : TextView by lazy {findViewById(R.id.QuoteMainActivity_editQuoteTextView)}
+
+    //sharedPreference
+    private val pref: SharedPreferences by lazy{getSharedPreferences("quotes", Context.MODE_PRIVATE)}
+    //model
+    private lateinit var quotes: MutableList<Quote>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quote_main)
+        initQuotes()
+
+        quotes = Quote.getQuotesFromPreference(pref)
+        this.randomQuote()
+
+        viewQuoteButton.setOnClickListener { randomQuote() }
+    }
+
+    fun initQuotes(){
+        val initialized = pref.getBoolean("init",false)
+        if(!initialized){
+            Quote.saveToPreference(pref,0,"창조적인 삶을 살려면 내가 틀릴지도 모른다는 공포를 버려야 한다.",)
+            Quote.saveToPreference(pref,1, "성공한 사람이 되려고 노력하기보다 가치있는 사람이 되려고 노력하라.", "알버트 아인슈타인")
+            Quote.saveToPreference(pref,2, "괴로운 시련처럼 보이는 것이 뜻밖의 좋은 일일 때가 많다.", "오스카 와일드")
+            Quote.saveToPreference(pref,3, "추구할 수 있는 용기가 있다면 우리의 모든 꿈은 이뤄질 수 있다.", "월트 디즈니")
+            Quote.saveToPreference(pref,4,"실패에서부터 성공을 만들어 내라. 좌절과 실패는 성공으로 가는 가장 확실한 디딤돌이다.","데일 카네기")
+
+            val editor = pref.edit()
+            editor.putBoolean("init",true)
+            editor.apply()
+        }
+    }
+
+    fun randomQuote(){
+        var randomIndex= Random().nextInt(quotes.size)
+        var randomQuote= quotes[randomIndex]
+
+        if(quotes.isEmpty()){
+            quoteTextView.setText("아직 명언 데이터가 없습니다.")
+            quoteAuthorTextView.setText("-")
+        }else {
+            quoteTextView.setText(randomQuote.text)
+            quoteAuthorTextView.setText(randomQuote.from)
+        }
+    }
+
+}
